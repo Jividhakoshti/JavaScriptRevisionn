30406fb7b4611b5313cf82941ab09550a3e0cbcb
Jividhakoshti <168750686+Jividhakoshti@users.noreply.github.com>
2025-05-19 12:27:58 +0530

    Create script.js

---
+++ Day 8/Practice/script.js
@@ -0,0 +1,41 @@
+// 🔹 4. Practice Projects
+// Counter:
+
+// 1.Create a simple counter with 3 buttons: Increment, Decrement, Reset.
+// 3.Change the counter color: green for positive, red for negative, black for zero.
+// let val = document.getElementById("val")
+// let btn1 = document.querySelector("#btn1")
+// let btn2 = document.querySelector("#btn2")
+// let btn3 = document.querySelector("#btn3")
+
+// let count = 0;
+// btn1.addEventListener("click",()=>{
+//     count++;
+//     val.textContent = count;
+//      val.style.color = 'green'
+// })
+// btn2.addEventListener("click",()=>{
+//     count--;
+//     val.textContent = count;
+//      val.style.color = 'red'
+// })
+// btn3.addEventListener("click",()=>{
+//     count = 0;
+//     val.textContent = count;
+//      val.style.color = "black"
+// })
+
+
+
+
+//4. Light/Dark Theme Toggle:
+
+// 5.Create a toggle button that switches the page between light mode and dark mode.
+
+// 6.Use classList.toggle() to switch between .light-theme and .dark-theme on the body.
+
+// 7.Change background color, text color accordingly. 
+// document.getElementById("toggleBtn").addEventListener("click", function () {
+//   document.body.classList.toggle("dark-theme");
+//   document.body.classList.toggle("light-theme");
+// });
