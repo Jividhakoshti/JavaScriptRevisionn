b3e267c4672b02e72dbde9387954281cc57fe7bd
Jividhakoshti <168750686+Jividhakoshti@users.noreply.github.com>
2025-05-20 09:07:29 +0530

    Create script.js

---
+++ DAY9/Change&InputEvents/script.js
@@ -0,0 +1,39 @@
+// //1.Input Event
+// // Show live input text in a paragraph.
+
+// // Display the number of characters typed.
+
+// // Convert typed text to uppercase and show it live.
+
+// let input = document.getElementById("input")
+// let p = document.getElementById("Live-text");
+// let charcount = document.getElementById("charcount")
+// let uppercase = document.getElementById("uppercase")
+
+// input.addEventListener("input",()=>{
+//     const value = input.value;
+//     p.textContent = value;
+
+//     charcount.textContent = value.length
+//     uppercase.textContent = value.toUpperCase();
+// })
+
+
+
+// //2.change event
+// Change background color when a dropdown option is selected.
+
+// Display selected value of dropdown below it.
+
+
+// let select = document.getElementById("select")
+// let body = document.querySelector("body")
+// let selectedcolor = document.getElementById("selectedcolor")
+
+// select.addEventListener("change",()=>{
+//     const selected = select.value
+//     body.style.backgroundColor = selected
+//     selectedcolor.textContent = selected
+// })
+
+
