c0cfbbd45cfa6fffcb4e9f7d6efcf24881b48928
Jividhakoshti <168750686+Jividhakoshti@users.noreply.github.com>
2025-05-20 09:05:50 +0530

    Create script.js

---
+++ DAY9/Clickevent/script.js
@@ -0,0 +1,52 @@
+// 1. Click Event
+// 💡 Questions:
+// 1.Change the text of a paragraph when a button is clicked.
+// let btn = document.querySelector("button")
+// let p = document.getElementById("para")
+
+// btn.addEventListener("click",()=>{
+//     p.textContent = "Byeee!!"
+// })
+
+
+// 2.Add a number to a counter every time a button is clicked.
+// let btn = document.querySelector("button")
+// let val = document.getElementById("val")
+// let count = 0
+// btn.addEventListener("click",()=>{
+//     val.textContent = count++
+// })
+
+
+// 3.Toggle the visibility of a paragraph on button click.
+// let btn = document.querySelector("button")
+// let para = document.getElementById("togle")
+
+
+// btn.addEventListener("click",()=>{
+//    if( para.style.display === "none"){
+//     para.style.display = "block"
+//    }
+//    else{
+//     para.style.display = "none"
+//    }
+// })
+
+
+//4. Append new list items to a list on each button click.
+
+// let btn = document.querySelector("button");
+// let list = document.getElementById("list")
+// let count = 1;
+
+// btn.addEventListener("click",()=>{
+//     const li = document.createElement("li");
+//     li.textContent = "Item"+ count;
+//     list.appendChild (li);
+//     count++;
+
+// })
+
+
+
+
