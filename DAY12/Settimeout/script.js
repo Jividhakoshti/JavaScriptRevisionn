873c5a942418cde586d4a28c3f8ce43ab74e24d2
Jividhakoshti <168750686+Jividhakoshti@users.noreply.github.com>
2025-05-24 09:17:33 +0530

    Create script.js

---
+++ DAY12/Settimeout/script.js
@@ -0,0 +1,25 @@
+// // ✅ 1. setTimeout – Delayed Execution
+// // Print “Hello after 3 seconds” using setTimeout.
+// setTimeout(print,3000);
+//  function print(){
+//         console.log("Hello")
+//     }
+
+// // Create a function that accepts a message and delay, then logs the message after the delay.
+// function printMsg(msg,delay){
+//     setTimeout(()=>{
+//         console.log(msg)
+//     },delay)
+// }
+// printMsg("Happiee Birthdayy!!",2000)
+
+// // Change the background color of the page to red after 2 seconds.
+// let body = document.querySelector("body");
+
+// setTimeout(()=>{
+//     body.style.backgroundColor = "Red"
+// },3000)
+
+
+
+
