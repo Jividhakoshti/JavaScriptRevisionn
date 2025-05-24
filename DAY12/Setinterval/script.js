dee5e34fa5fafca0cc187f0bc56d09d307e9255d
Jividhakoshti <168750686+Jividhakoshti@users.noreply.github.com>
2025-05-24 09:18:30 +0530

    Create script.js

---
+++ DAY12/Setinterval/script.js
@@ -0,0 +1,42 @@
+// Print “Tick” every second using setInterval.
+
+// setInterval(()=>{
+//     console.log("Tick")
+// },1000)
+
+// Create a digital clock that updates every second.
+
+// Make a counter that increases every 2 seconds.
+// let count = 0;
+//  setInterval(()=>{
+//     count++
+//     console.log("Count : " , count)
+    
+//  },2000)
+
+// Alternate background color between white and gray every second.
+// let body = document.querySelector("body");
+// let bgwhite = true
+//  setInterval(()=>{
+//    if(bgwhite){
+//     body.style.backgroundColor = "grey"
+//    }
+//    else{
+//     body.style.backgroundColor = "white"
+//    }
+//    bgwhite = !bgwhite;
+//  },1000)
+
+
+// Create a blinking text effect using setInterval
+// let p = document.querySelector("#p");
+
+// setInterval(()=>{
+//     if(p.style.visibility==="hidden"){
+//         p.style.visibility="visible"
+//     }
+//     else{
+//         p.style.visibility = "hidden";
+//     }
+
+// },500)
